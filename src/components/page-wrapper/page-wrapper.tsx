a7a14fb5b4486c878e75903616facbd6e864315f
Вечканов Алексей <avechkanov@alfabank.ru>
2022-07-24 23:07:32 +0300

    added page wrapper component. It will be used for all pages - it will contain common things (header footer etc)

---
+++ src/components/page-wrapper/page-wrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+ 
+export const PageWrapper: React.FunctionComponent<Props> = (props) => {
+    return (
+        <div>
+            { props.children }
+        </div>
+    );
+}
