95bf33b82d76123aedf707bd162d106737cc3a92
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-07-26 22:22:43 +0300

    WH-71 added static page - user

---
+++ src/types/userInfo.ts
@@ -0,0 +1,10 @@
+export type TUserInfo = {
+    userId: string;
+    id: number;
+    avatar_url: string;
+    login: number;
+    email: string;
+    location: string;
+    created_at: string;
+    message: string;
+}
