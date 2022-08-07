286db4b1adf5854594a24ca33f676e788c5ce8a9
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-08-07 19:53:42 +0300

    changed BannerButtons

--- src/components/banner-buttons/BannerButtons.tsx
+++ src/components/banner-buttons/BannerButtons.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { MainPageLink } from "../UI/main-page-link";
 import HTMLImage1 from "@/public/html-directory.svg";
 import styles from "./banner-main-page.module.scss";
@@ -14,7 +13,7 @@ export const bannerButtons: React.FunctionComponent<Props> = (props) => {
       <MainPageLink
         title="HTML справочник"
         subTitle="Все HTML теги"
-        Image={HTMLImage1}
+        image={HTMLImage1}
         url="/html-list"
       />
     </div>
